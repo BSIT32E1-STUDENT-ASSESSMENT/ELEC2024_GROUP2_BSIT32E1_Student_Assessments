286e5de8915d824f8fa9c0c83ee4299a34a346db
AndoJoshuaBSIT32E1 <160818162+AndoJoshuaBSIT32E1@users.noreply.github.com>
2024-05-30 13:51:27 +0800

    Add project files.

---
+++ Student_Assessment_Projects/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Student_Assessment_Projects.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
