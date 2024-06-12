24cf436fe066d11fe035874064988e95116640dd
kateashely <kateashleyvaldez19@gmail.com>
2024-06-12 23:09:12 +0800

    remove student grade

--- Student_Assessment_Projects/Models/StudentGrade.cs
+++ Student_Assessment_Projects/Models/StudentGrade.cs
@@ -1,14 +1,1 @@
-﻿namespace Student_Assessment_Projects.Models
-{
-    public class StudentGrade
-    {
-            public string SubjectCode { get; set; }
-            public string Subject { get; set; }
-            public int FinalGrade7 { get; set; }
-            public int FinalGrade8 { get; set; }
-            public int FinalGrade9 { get; set; }
-            public int FinalGrade10 { get; set; }
-            public string Strand { get; set; }
-        
-    }
-}
+﻿
