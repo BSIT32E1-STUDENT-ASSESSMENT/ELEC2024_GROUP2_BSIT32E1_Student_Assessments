2b084b43912c1fb8885e843c1125ec190df1fb1a
Arandia-AericaMae-BSIT32E1 <arandiaaerica@gmail.com>
2024-06-13 22:44:11 +0800

    updates the ApplicationdbContext.cs

--- Student_Assessment_Projects/Data/ApplicationDbContext.cs
+++ Student_Assessment_Projects/Data/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Student_Assessment_Projects.Models;
+
 
 namespace Student_Assessment_Projects.Data
 {
@@ -8,6 +10,16 @@ namespace Student_Assessment_Projects.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Additional configurations if needed
         }
     }
 }
