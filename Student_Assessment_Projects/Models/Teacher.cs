c05a1b683a776f637fc862d0815154ac1baf5aca
Arandia-AericaMae-BSIT32E1 <arandiaaerica@gmail.com>
2024-06-13 22:42:57 +0800

    added new model named Teacher.cs

--- Student_Assessment_Projects/Models/Teacher.cs
+++ Student_Assessment_Projects/Models/Teacher.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using Microsoft.AspNetCore.Identity;
+namespace Student_Assessment_Projects.Models
+{
+    public class Teacher : IdentityUser
+    {
+        public string FullName { get; set; }
+    }
+}
+
