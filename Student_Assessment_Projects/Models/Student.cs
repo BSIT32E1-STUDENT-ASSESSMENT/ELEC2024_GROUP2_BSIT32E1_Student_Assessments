21ecd3d8bb5e74dcd78ebc6ec310ee3e2456c82d
Arandia-AericaMae-BSIT32E1 <arandiaaerica@gmail.com>
2024-06-13 22:42:15 +0800

    added new model named Student.cs

--- Student_Assessment_Projects/Models/Student.cs
+++ Student_Assessment_Projects/Models/Student.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace Student_Assessment_Projects.Models
+{
+    public class Student : IdentityUser
+    {
+        public string FullName { get; set; }
+        public ICollection<Grade> Grades { get; set; }
+    }
+}
