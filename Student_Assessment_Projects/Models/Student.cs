d848471ae65e14bc599b3718724121069939301d
John Gaylord Mantal <145678314+Mantal-JohnGaylord-BSIT32E1@users.noreply.github.com>
2024-06-21 21:30:21 +0800

    UpdateStudent

--- Student_Assessment_Projects/Models/Student.cs
+++ Student_Assessment_Projects/Models/Student.cs
@@ -5,6 +5,9 @@ namespace Student_Assessment_Projects.Models
 {
     public class Student : IdentityUser
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Strand { get; set; }
         public string FullName { get; set; }
         public ICollection<Grade> Grades { get; set; }
     }
