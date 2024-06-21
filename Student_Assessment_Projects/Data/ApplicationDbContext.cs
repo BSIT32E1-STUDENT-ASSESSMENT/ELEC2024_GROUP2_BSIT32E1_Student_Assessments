5a6c6dfa671e5cbc10aa4c0578ff517c503d0f40
John Gaylord Mantal <145678314+Mantal-JohnGaylord-BSIT32E1@users.noreply.github.com>
2024-06-21 21:45:11 +0800

    UpdateApplicationDbContext

--- Student_Assessment_Projects/Data/ApplicationDbContext.cs
+++ Student_Assessment_Projects/Data/ApplicationDbContext.cs
@@ -5,6 +5,16 @@ using Student_Assessment_Projects.Models;
 
 namespace Student_Assessment_Projects.Data
 {
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StudentGrade> StudentGrades { get; set; }
+    }
+
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
@@ -15,6 +25,7 @@ namespace Student_Assessment_Projects.Data
         public DbSet<Student> Students { get; set; }
         public DbSet<Teacher> Teachers { get; set; }
         public DbSet<Grade> Grades { get; set; }
+        public DbSet<StudentGrade> StudentGrades { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
